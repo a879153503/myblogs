9fd80be06f42b204328c8af9e435c4952c499b71
qiaoperng <879153503@qq.com>
2020-07-09 18:59:24 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: '乔鹏的博客',
+    head: [
+        ['link', {rel: 'icon', href: '/img/logo.jpg'}]
+    ],
+    themeConfig: {
+        logo: '/img/logo.jpg',
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '百度', link: 'http://www.baidu.com'},
+            {text: '谷歌', link: 'http://www.itboyhub.com'}
+        ],
+        sidebar: 'auto'
+    }
+};
